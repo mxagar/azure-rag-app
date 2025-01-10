8f0010476e5d17883a66bb99c208452172d4c1f3
Mikel Sagardia <mxagar@gmail.com>
2025-01-10 12:39:36 +0100

    feat: clean up and tests

--- backend/api.py
+++ backend/api.py
@@ -1,89 +1,133 @@
 """Description.
 
-Original source: https://github.com/alfredodeza/azure-rag
+Original idea/source: https://github.com/alfredodeza/azure-rag
+(But the code was considerable changed)
 
-Usage (local):
+Run (local):
 
-    uvicorn --env-file ../.env --host 0.0.0.0 backend.api:app
+    # Launch FastAPI app locally
+    cd .../azure-rag-app
+    uvicorn --env-file .env --host 0.0.0.0 --port 8080 backend.api:app
+
+    # Open browser and go to http://localhost:8080/docs
+    # Go to /ask and click on 'Try it out'
+    # Enter a question and click on 'Execute', e.g.:
+    # "What is the best Bourbon Barrel wine?"
+    # Check response in Web UI
+
+Use:
+
+    import requests
+
+    # Define the URL and payload
+    url = "http://127.0.0.1:8080/ask"
+    payload = {"query": "Which is the best wine?"}
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the POST request
+    response = requests.post(
+        url,
+        json=payload,
+        headers=headers,
+        proxies={"http": None, "https": None}  # Use this only if you have any proxies...
+    )
+
+    if response.status_code == 200:
+        print(response.json().get("response"))
+    else:
+        print("Error:", response.status_code)
 
 Author: Mikel Sagardia
 Date: 2025-01-10
 """
-
 import os
-from os.path import dirname
+import pathlib
 from dotenv import load_dotenv
 from pydantic import BaseModel
+import yaml
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-import openai
 from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
-from langchain.vectorstores import AzureSearch
+from langchain_community.vectorstores import AzureSearch
 from langchain_community.retrievers import AzureAISearchRetriever
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
 
-# Load environment variables
-current_dir = os.path.abspath(".")
-root_dir = dirname(current_dir)
-env_file = os.path.join(root_dir, '.env')
-env_file_loaded = False
+# Load environment variables (local execution)
+CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
+ROOT_DIR = CURRENT_DIR.parent
+ENV_FILE = os.path.join(ROOT_DIR, '.env')
 try:
-    env_file_loaded = load_dotenv(env_file, override=True)
-except Exception as e:
+    _ = load_dotenv(ENV_FILE, override=True)
+except Exception:
     pass
 
 
-# Retrieve Azure credentials and variables
-azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-azure_openai_endpoint_uri = os.getenv("AZURE_OPENAI_ENDPOINT_URI")
-embedding_deployment_name = os.getenv("EMBEDDING_DEPLOYMENT_NAME")
-chat_deployment_name = os.getenv("CHAT_DEPLOYMENT_NAME")
-azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
-azure_openai_api_version = os.getenv("AZURE_OPENAI_API_VERSION")
-azure_search_api_key = os.getenv("AZURE_SEARCH_API_KEY")
-azure_search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
-azure_search_index_name = os.getenv("AZURE_SEARCH_INDEX_NAME")
+# Retrieve variables
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+AZURE_OPENAI_ENDPOINT_URI = os.getenv("AZURE_OPENAI_ENDPOINT_URI")
+EMBEDDING_DEPLOYMENT_NAME = os.getenv("EMBEDDING_DEPLOYMENT_NAME")
+CHAT_DEPLOYMENT_NAME = os.getenv("CHAT_DEPLOYMENT_NAME")
+AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
+AZURE_SEARCH_ENDPOINT = os.getenv("AZURE_SEARCH_ENDPOINT")
+AZURE_SEARCH_INDEX_NAME = os.getenv("AZURE_SEARCH_INDEX_NAME")
 
+
+# Load configuration
+def load_config(config_path: str | pathlib.Path) -> dict:
+    if isinstance(config_path, pathlib.Path):
+        config_path = str(config_path)
+    try:
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+    except Exception as e:
+        print(f"Error loading config.yaml: {e}")
+
+    return config
+
+config_path = CURRENT_DIR / "config.yaml"
+config = load_config(config_path=config_path)
 
 # Connect to Azure OpenAI - Embeddings Model
 embeddings = AzureOpenAIEmbeddings(
-    deployment=embedding_deployment_name,
-    azure_endpoint=azure_openai_endpoint,  # Short/base URL
-    api_version=azure_openai_api_version,
-    api_key=azure_openai_api_key,
+    deployment=EMBEDDING_DEPLOYMENT_NAME,
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,  # Short/base URL
+    api_version=AZURE_OPENAI_API_VERSION,
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     chunk_size=1
 )
 
 # Connect to Azure AI Search
 search = AzureSearch(
-    azure_search_endpoint=azure_search_endpoint,
-    azure_search_key=azure_search_api_key,
-    index_name=azure_search_index_name,
+    azure_search_endpoint=AZURE_SEARCH_ENDPOINT,
+    azure_search_key=os.getenv("AZURE_SEARCH_API_KEY"),
+    index_name=AZURE_SEARCH_INDEX_NAME,
     embedding_function=embeddings.embed_query,
 )
 
 # Connect to Azure AI Search Retriever
 retriever = AzureAISearchRetriever(
     content_key="content",
-    api_key=azure_search_api_key,
-    index_name=azure_search_index_name,
-    service_name=azure_search_endpoint,
+    api_key=os.getenv("AZURE_SEARCH_API_KEY"),
+    index_name=AZURE_SEARCH_INDEX_NAME,
+    service_name=AZURE_SEARCH_ENDPOINT,
     top_k=1,
 )
 
 # Initialize Azure OpenAI - Chat Model
 chat_model = AzureChatOpenAI(
-    azure_endpoint=azure_openai_endpoint_uri, # Long URI, not the base
-    openai_api_version=azure_openai_api_version,
-    openai_api_key=azure_openai_api_key,
-    temperature=0.7,
-    max_tokens=2000,
-    top_p=0.8,
+    azure_endpoint=AZURE_OPENAI_ENDPOINT_URI,  # Long URI, not the base
+    openai_api_version=AZURE_OPENAI_API_VERSION,
+    openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    temperature=config.get("chat_temperature", 0.7),
+    max_tokens=config.get("chat_max_tokens", 2000),
+    top_p=config.get("chat_top_p", 0.8),
 )
 
 
@@ -136,39 +180,3 @@ def chatbot(query: str):
     response = chain.invoke(query)
 
     return response
-
-
-### ---
-
-
-def search(query):
-    """
-    Send the query to Azure Cognitive Search and return the top result
-    """
-    docs = search.similarity_search_with_relevance_scores(
-        query=query,
-        k=5,
-    )
-    result = docs[0][0].page_content
-    print(result)
-    return result
-
-
-def assistant(query, context):
-    messages=[
-        # Set the system characteristics for this chat bot
-        {"role": "system", "content": "Asisstant is a chatbot that helps you find the best wine for your taste."},
-
-        # Set the query so that the chatbot can respond to it
-        {"role": "user", "content": query},
-
-        # Add the context from the vector search results so that the chatbot can use
-        # it as part of the response for an augmented context
-        {"role": "assistant", "content": context}
-    ]
-
-    response = openai.ChatCompletion.create(
-        engine="demo-alfredo",
-        messages=messages,
-    )
-    return response['choices'][0]['message']['content']
